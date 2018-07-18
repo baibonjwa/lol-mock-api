12af3a5149e1c2eb0310cbba0e830d781f933b63
happybai <byron.bai@aol.com>
2018-07-18 16:28:05 +0800

    Added app-check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# ApplicationController
 class ApplicationController < ActionController::API
+  def app_check
+    render json: {
+      data: {
+        sha: `git rev-parse HEAD`
+      }
+    }
+  end
 end
